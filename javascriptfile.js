e1691c43df90fbcc80258eff753f1bdb557cae78
karla2aguilar <karla.5225.aguilar@gmail.com>
2018-08-02 15:36:00 -0400

    Added Personal Faves

---
+++ javascriptfile.js
@@ -0,0 +1,11 @@
+function resize(image){
+    if () {
+        image.height = "50";
+        image.width = "50";
+    } else {
+        image.height = "100";
+        image.width = "100";
+}
+
+
+document.getElementById("images").addEventListener("mouseover", );
