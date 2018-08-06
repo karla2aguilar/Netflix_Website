259cbefb9e5130d9f66bd91e6e0655eda0aa4b7b
karla2aguilar <karla.5225.aguilar@gmail.com>
2018-08-06 15:35:47 -0400

    Made site more interactive

--- javascriptfile.js
+++ javascriptfile.js
@@ -1,11 +1,29 @@
-function resize(image){
-    if () {
-        image.height = "50";
-        image.width = "50";
-    } else {
-        image.height = "100";
-        image.width = "100";
-}
 
 
-document.getElementById("images").addEventListener("mouseover", );
+
+
+// document.addEventListener('DOMContentLoaded', function (e) {
+//     var image = document.getElementById('images');
+//     var image = document.querySelectorAll("#images")
+//     image.width(100);
+//     image.height(100);
+//     console.log("IT'S LOADEDDDDDDDDDDDDDD");
+    
+//     var random = true;
+
+//     image.addEventListener('mouseover', function (e) {    
+//         if (random) {
+//             console.log("working")
+//             image.style.height = "150px";
+//             image.style.width = "150px";
+//             random = false;
+//         } else {
+//             image.style.height = "100px";
+//             image.style.width = "100px";
+//             random = true;
+//         }
+//     })
+
+
+
+// })
